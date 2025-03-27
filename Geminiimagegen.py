3937c8fcbcf2f072ccc24cd73bccaa1ad2b80894
Umakant Biswal <141271063+umakantiiit@users.noreply.github.com>
2025-03-27 09:29:49 +0530

    Update Geminiimagegen.py

--- Geminiimagegen.py
+++ Geminiimagegen.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import google.generativeai as genai
-from google.genai import types
+from google.generativeai import types
 from PIL import Image
 from io import BytesIO
 import base64
